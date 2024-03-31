4f2e4e5a8780c1babe9429dd76956f9fdbb8f404
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-31 08:10:36 -0300

    feat: create components Game and GameOver

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useCallback, useEffect, useState } from "react";
+
 import "./App.css";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
 import StartScreen from "./components/StartScreen";
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
