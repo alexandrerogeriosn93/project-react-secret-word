880c5cde08e03bf8de683483da26e34852e0df38
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-31 12:54:23 -0300

    feat: add Game Over layout jsx and styles css

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
           score={score}
         />
       )}
-      {gameStage === "end" && <GameOver retry={retry} />}
+      {gameStage === "end" && <GameOver retry={retry} score={score} />}
     </div>
   );
 }
